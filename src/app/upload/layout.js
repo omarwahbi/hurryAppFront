54b4e5711bef48c3a0237495069007e1a30f5896
asr <asr12211@outlook.com>
2024-01-12 17:47:08 +0100

    ADD: layout to upload page for SEO

---
+++ src/app/upload/layout.js
@@ -0,0 +1,10 @@
+export const metadata = {
+   title: "Upload",
+   description: 'Upload a video to our website',
+ }
+
+export default function DashboardLayout({ children }) {
+   return <>
+      {children}
+   </>;
+ }
