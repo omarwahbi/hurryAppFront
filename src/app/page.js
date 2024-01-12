93cae0cb151bc75e97e7fe9c24b0b81656a35ae2
omarwahbi <99210606+omarwahbi@users.noreply.github.com>
2024-01-12 15:12:30 +0300

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Stream Spectra!</h1>
+      <p>
+        Welcome to StreamSpectra, where movie magic meets shared moments! Dive
+        into an immersive cinematic experience with your friends, where everyone
+        has the power to control the show. StreamSpectra is your ticket to
+        seamless movie nights, offering a dynamic platform for sharing,
+        exploring, and enjoying your favorite films together. Elevate your
+        movie-watching escapades with the freedom for everyone to take the
+        director's seat. Get ready to create unforgettable memories – it's movie
+        time, redefined!
+      </p>
+      <div></div>
+    </div>
+  );
+}
