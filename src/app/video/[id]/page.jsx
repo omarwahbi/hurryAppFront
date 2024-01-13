26e934ced7f5df2937c357ff373a8f347583527c
asr <asr12211@outlook.com>
2024-01-13 14:45:14 +0100

    ADD: video

--- src/app/video/[id]/page.jsx
+++ src/app/video/[id]/page.jsx
@@ -1,24 +1,28 @@
 "use client";
 
-import MessageComponent from "@/components/message";
-import Image from "next/image";
-import Link from "next/link";
 import { useParams, useSearchParams } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
+import Cookies from "js-cookie";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import MessageComponent from "@/components/message";
+import useIsIntersecting from "@/hooks/useIntersecting";
 
 function VideoPage() {
 
-   // TDOD: get user data. Dependant on back-end and where to store user data (localStorage, cookies, context)
-
-   const SERVER_URL = "/";
+   const SERVER_URL = "http://192.168.4.90:30010/"; // TODO: change
    const videoId = useParams().id;
    const searchParams = useSearchParams();
  
    const isPartyText = searchParams.get("party");
+   const streamId = searchParams.get("streamId");
 
    const videoRef = useRef(null);
    const [ws, setWs] = useState(null);
    const isUpdating = useRef(false);
+   const isSeeking = useRef(false);
 
    const [messages, setMessages] = useState([{message: "First Message", name: "Namee"}, {message: "First Message", name: "Namee"}, {message: "First Message", name: "Namee"}, {message: "First Message", name: "Namee"}, {message: "First Message", name: "Namee"}, {message: "First Message", name: "Namee"}, {message: "First Message", name: "Namee"}, {message: "First Message", name: "Namee"}]);
    const textInputRef = useRef(null);
@@ -27,6 +31,19 @@ function VideoPage() {
    const [description, setDescription] = useState("Descriptionnnnnnnnnn jsndkjsnkjdn"); // TODO: should be an empty string
 
    const [isChatOn, setIsChatOn] = useState(false);
+   const bottomDivRef = useRef(null);
+   const scrollerRef = useRef(null);
+
+   const isIntersecting = useIsIntersecting(bottomDivRef);
+
+   // useEffect(() => {
+   //    // TODO: get video
+   //    fetch(`${SERVER_URL}/`).then(response => {
+   //       if (response.ok) return response.json();
+   //    }).then(() => {
+   //       // TODO: set videoId, title and description
+   //    })
+   // }, [videoId]);
 
    useEffect(() => {
       if (isPartyText !== "true") return;
@@ -34,10 +51,13 @@ function VideoPage() {
       const websocket = new WebSocket("ws://localhost:3000");
       setWs(websocket);
 
+      websocket.onopen = () => {
+         websocket.send(JSON.stringify({ type: 'JOIN', streamId }));
+      };
+
       websocket.onmessage = (event) => {
          isUpdating.current = true;
          const data = JSON.parse(event.data);
-         // console.log(data.type);
 
          switch (data.type) {
             case "PLAY":
@@ -48,7 +68,17 @@ function VideoPage() {
                videoRef.current.currentTime = data.currentTime;
                videoRef.current.pause();
                break;
-            // case for SEEK
+            // case 'SEEK':
+            //    if (!isSeeking.current) {
+            //       videoRef.current.currentTime = data.currentTime;
+            //       videoRef.current.play();
+            //    }
+            //    break;
+            // case 'CHAT_MESSAGE':
+            //    setMessages((prevMessages) => [...prevMessages, da-++ta]);
+            //    break;
+            // default:
+            //    console.log("Received unknown message type:", data.type);
          }
          setTimeout(() => (isUpdating.current = false), 300); // Adjust delay as needed
       };
@@ -56,7 +86,7 @@ function VideoPage() {
       return () => {
          websocket.close();
       };
-   }, [isPartyText]);
+   }, [isPartyText, streamId]);
 
    function handlePlay() {
       if (isPartyText !== "true") return;
@@ -75,11 +105,39 @@ function VideoPage() {
    }
 
    function handleSendMessage() {
+      if (isPartyText !== "true") return;
+      const text = textInputRef.current?.value;
+
+      if (!text) return;
+
+      const message = {
+         type: 'CHAT_MESSAGE',
+         name: Cookies.get('username'),
+         content: text,
+         timestamp: videoRef.current?.currentTime || 0
+      };
+      setMessages((prevMessages) => [...prevMessages, message]);
+      ws?.send(JSON.stringify(message));
+      textInputRef.current.value = "";
+      
+      if (isIntersecting) {
+         scrollerRef.current.scrollIntoView({ behavior: "smooth" });
+      }
       
    }
 
-   // TODO: add seek handling when back end complete
-   // function handleSeek() {}
+   function handleSeek() {
+      if (isPartyText !== "true") return;
+      if (!isUpdating.current && ws) {
+         isSeeking.current = true;
+         videoRef.current.currentTime = seekTime;
+         videoRef.current.play();
+         ws.send(JSON.stringify({ type: 'SEEK', currentTime: seekTime }));
+         setTimeout(() => {
+            isSeeking.current = false;
+         }, 300);
+      }
+   }
 
 
    return <div className="overflow-hidden md:w-[60%] h-[100svh]">
@@ -90,11 +148,20 @@ function VideoPage() {
             controls
             onPlay={handlePlay}
             onPause={handlePause}
+            // onClick={(e) => {
+            //    const rect = e.currentTarget.getBoundingClientRect();
+            //    const offsetX = e.clientX - rect.left;
+            //    const width = rect.width;
+            //    const seekTime = (offsetX / width) * videoRef.current.duration;
+            //    handleSeek(seekTime);
+            // }}
+            muted
          >
             <source src={`${SERVER_URL}${videoId}.mp4`} type="video/mp4" />
             Your browser does not support the video tag.
          </video>
       </main>
+      <div className="relative h-[60%] md:h-[100%]">
       <div className="relative h-full">
          <section className="py-3 px-5">
             <h2 className="mb-3 text-xl">{title}</h2>
@@ -103,7 +170,7 @@ function VideoPage() {
                   <center><Image src="/icons/edit.png" alt="edit icon" width={35} height={35} className="mb-1" /></center>
                   Edit
                </Link>}
-               {isPartyText !== "true" ? <button className="w-fit mx-auto"> {/* only if it's not a watch party */}
+               {isPartyText !== "true" ? <button className="w-fit mx-auto" > {/* only if it's not a watch party */}
                   <center><Image src="/icons/live.png" alt="edit icon" width={35} height={35} className="mb-1" /></center>
                   Create Watch Party
                </button>
@@ -115,18 +182,19 @@ function VideoPage() {
             <p className="text-lg mt-5 mb-2">Description</p>
             <p className="">{description}</p>
          </section>
-         <section className={`no-scrollbar bg-red-300 px-5 md:fixed absolute left-0 right-0 transition-all ${isPartyText === "true" && isChatOn ? "top-0 bottom-0 h-full" : "top-[100%] bottom-0 h-0"} md:left-[60%] md:right-0`}>
-            <div className="no-scrollbar overflow-y-scroll h-[80%] bg-blue-300">
+         <section className={`no-scrollbar px-5 md:fixed absolute left-0 right-0 transition-all ${isPartyText === "true" && isChatOn ? "top-0 bottom-0 h-full" : "top-[100%] bottom-0 h-0"} md:left-[60%] md:right-0`}>
+            <div className="no-scrollbar overflow-y-scroll h-[80%] bg-blue-300" ref={scrollerRef}>
                {messages.map((message, i) =>
                   <MessageComponent
                      key={i}
-                     message={message.message /* or message.text */}
-                     username={message.name /* or message.senderName or username */}
+                     message={message.content}
+                     username={message.name}
                   />)}
+               <div className="w-full h-3" ref={bottomDivRef} />
             </div>
             <div className=" h-10" />
-            <div className={`w-full ${isPartyText === "true" && isChatOn ? "fixed bottom-0" : ""}`}>
-               <div className="mb-5 grid grid-cols-[55%_35%] md:grid-cols-1 align-middle justify-center">
+            <div className={`w-full opacity-0 bg-white ${isPartyText === "true" && isChatOn ? "fixed bottom-0 opacity-100" : ""}`}>
+               <div className="mb-5 grid grid-cols-[50%_35%] md:grid-cols-1 align-middle justify-center">
                   <input
                      ref={textInputRef}
                      type="text"
@@ -134,14 +202,15 @@ function VideoPage() {
                      id="message"
                      className="w-full md:w-[35%] md:my-2 px-2 py-1 mr-3 border-[1px] rounded-md border-gray-300 focus:border-gray-500 active:border-gray-500 focus:outline-none active:outline-none"
                   />
-                  <div className="mx-5 h-full">
-                     <button className="mr-5 px-2 h-full rounded-md w-fit bg-indigo-500">Send</button>
+                  <div className="ml-5 h-full">
+                     <button className="mr-3 px-2 h-full rounded-md w-fit bg-indigo-500" onClick={handleSendMessage}>Send</button>
                      <button className="px-2 md:py-1 h-full rounded-md w-fit bg-red-500" onClick={(e) => setIsChatOn(false)}>Close</button>
                   </div>
                </div>
             </div>
          </section>
-      </div>
+         </div>
+         </div>
          
    </div>;
 }
