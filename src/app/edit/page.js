38664b11f1d6c5ed8cc157ed629694c7ed753ce4
asr <asr12211@outlook.com>
2024-01-13 11:24:27 +0300

    EDIT: some code edit in upload & edit pages

--- src/app/edit/page.js
+++ src/app/edit/page.js
@@ -1,8 +1,14 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 function UploadPage() {
+
+   
+   const searchParams = useSearchParams();
+ 
+   const isPartyText = searchParams.get("videoId");
 
    const [error, setError] = useState("");
    const [isLoading, setIsLoading] = useState(false);
@@ -35,9 +41,21 @@ function UploadPage() {
          <div className="mx-5">
             <form onSubmit={handleSubmit}>
                <label htmlFor="title" className="w-fit block text-gray-900">Title</label>
-               <input type="text" name="title" id="title" className="px-2 py-1 mb-5 block border-[1px] rounded-md w-full border-gray-300 focus:border-gray-500 active:border-gray-500 focus:outline-none active:outline-none " />
+               <input
+                  required
+                  type="text"
+                  name="title"
+                  id="title"
+                  className="px-2 py-1 mb-5 block border-[1px] rounded-md w-full border-gray-300 focus:border-gray-500 active:border-gray-500 focus:outline-none active:outline-none "
+               />
                <label htmlFor="description" className="w-fit block">Description</label>
-               <input type="text" name="description" id="description" className="px-2 py-1 block mb-5 border-[1px] rounded-md w-full border-gray-300 focus:border-gray-500 active:border-gray-500 focus:outline-none active:outline-none" />
+               <input
+                  required
+                  type="text"
+                  name="description"
+                  id="description"
+                  className="px-2 py-1 block mb-5 border-[1px] rounded-md w-full border-gray-300 focus:border-gray-500 active:border-gray-500 focus:outline-none active:outline-none"
+               />
                <button type="submit" className="flex w-full text-lg justify-center rounded-md bg-indigo-600 px-3 py-2 font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                   Update
                </button>
--- src/app/upload/page.js
+++ src/app/upload/page.js
@@ -103,15 +103,27 @@ function UploadPage() {
                   </label>
                </div>
                <label htmlFor="title" className="w-fit block text-gray-900">Title</label>
-               <input type="text" name="title" id="title" className="px-2 py-1 mb-5 block border-[1px] rounded-md w-full border-gray-300 focus:border-gray-500 active:border-gray-500 focus:outline-none active:outline-none " />
+               <input
+                  required
+                  type="text"
+                  name="title"
+                  id="title"
+                  className="px-2 py-1 mb-5 block border-[1px] rounded-md w-full border-gray-300 focus:border-gray-500 active:border-gray-500 focus:outline-none active:outline-none "
+               />
                <label htmlFor="description" className="w-fit block">Description</label>
-               <input type="text" name="description" id="description" className="px-2 py-1 block mb-5 border-[1px] rounded-md w-full border-gray-300 focus:border-gray-500 active:border-gray-500 focus:outline-none active:outline-none" />
+               <input
+                  required
+                  type="text"
+                  name="description"
+                  id="description"
+                  className="px-2 py-1 block mb-5 border-[1px] rounded-md w-full border-gray-300 focus:border-gray-500 active:border-gray-500 focus:outline-none active:outline-none"
+               />
                <button type="submit" className="flex w-full text-lg justify-center rounded-md bg-indigo-600 px-3 py-2 font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                   Upload
                </button>
             </form>
-            <p>{ isLoading && "Loading..." }</p>
-            <p className="text-red-500">{ error }</p>
+            <p>{isLoading && "Loading..."}</p>
+            <p className="text-red-500">{error}</p>
          </div>
       </div>
    </>;
